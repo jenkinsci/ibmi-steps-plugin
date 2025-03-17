7cdbfce464fb6f193a0e23caa7c126634e457cef
Seb Julliand <sjulliand@arcadsoftware.com>
2025-03-17 17:23:38 +0100

    Use latest jackson2-api to produce CSV

--- src/main/java/org/jenkinsci/plugins/ibmisteps/model/SQLResult.java
+++ src/main/java/org/jenkinsci/plugins/ibmisteps/model/SQLResult.java
@@ -1,184 +1,208 @@
 package org.jenkinsci.plugins.ibmisteps.model;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import de.siegmar.fastcsv.writer.CsvWriter;
-
+import java.io.IOException;
 import java.io.Serializable;
 import java.io.StringWriter;
 import java.math.BigDecimal;
-import java.sql.*;
 import java.sql.Date;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SequenceWriter;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+
+import edu.umd.cs.findbugs.annotations.CheckForNull;
 
 public class SQLResult implements Serializable {
-    private static final long serialVersionUID = 6196545312618433171L;
+	private static final long serialVersionUID = 6196545312618433171L;
 
-    private final List<SQLColumn> columns = new LinkedList<>();
-    private final List<SQLRow> rows = new LinkedList<>();
+	private final List<SQLColumn> columns = new LinkedList<>();
+	private final List<SQLRow> rows = new LinkedList<>();
 
-    private final int updateCount;
+	private final int updateCount;
 
-    public SQLResult(final int updateCount) {
-        this.updateCount = updateCount;
-    }
+	public SQLResult(final int updateCount) {
+		this.updateCount = updateCount;
+	}
 
-    public SQLResult(ResultSet resultSet) throws SQLException {
-        this(0);
-        loadMetaData(resultSet.getMetaData());
-        while (resultSet.next()) {
-            loadRow(resultSet);
-        }
-    }
+	public SQLResult(final ResultSet resultSet) throws SQLException {
+		this(0);
+		loadMetaData(resultSet.getMetaData());
+		while (resultSet.next()) {
+			loadRow(resultSet);
+		}
+	}
 
-    private void loadMetaData(ResultSetMetaData metaData) throws SQLException {
-        final int columnCount = metaData.getColumnCount();
-        for (int i = 0; i < columnCount; i++) {
-            final int column = i + 1;
-            columns.add(new SQLColumn(
-                    metaData.getColumnName(column),
-                    metaData.getColumnTypeName(column),
-                    metaData.getPrecision(column),
-                    metaData.getScale(column)));
-        }
-    }
+	private void loadMetaData(final ResultSetMetaData metaData) throws SQLException {
+		final int columnCount = metaData.getColumnCount();
+		for (int i = 0; i < columnCount; i++) {
+			final int column = i + 1;
+			columns.add(new SQLColumn(
+					metaData.getColumnName(column),
+					metaData.getColumnTypeName(column),
+					metaData.getPrecision(column),
+					metaData.getScale(column)));
+		}
+	}
 
-    private void loadRow(ResultSet resultSet) throws SQLException {
-        final SQLRow row = new SQLRow();
-        for (int i = 0; i < getColumnCount(); i++) {
-            row.addCell(columns.get(i).getName(), resultSet.getObject(i + 1));
-        }
-        rows.add(row);
-    }
+	private void loadRow(final ResultSet resultSet) throws SQLException {
+		final SQLRow row = new SQLRow();
+		for (int i = 0; i < getColumnCount(); i++) {
+			row.addCell(columns.get(i).getName(), resultSet.getObject(i + 1));
+		}
+		rows.add(row);
+	}
 
-    public List<SQLColumn> getColumns() {
-        return columns;
-    }
+	public List<SQLColumn> getColumns() {
+		return columns;
+	}
 
-    public int getColumnCount() {
-        return columns.size();
-    }
+	public int getColumnCount() {
+		return columns.size();
+	}
 
-    public List<SQLRow> getRows() {
-        return rows;
-    }
+	public List<SQLRow> getRows() {
+		return rows;
+	}
 
-    public int getRowCount() {
-        return rows.size();
-    }
+	public int getRowCount() {
+		return rows.size();
+	}
 
-    public int getUpdateCount() {
-        return updateCount;
-    }
+	public int getUpdateCount() {
+		return updateCount;
+	}
 
-    public String toCSV() {
-        final StringWriter sw = new StringWriter();
-        final CsvWriter csvBuilder = CsvWriter.builder().build(sw);
-        //Write headers
-        csvBuilder.writeRecord(this.columns.stream().map(SQLColumn::getName).collect(Collectors.toList()));
-        //Write rows
-        for (final SQLRow row : rows) {
-            csvBuilder.writeRecord(row.getCells().values().stream().map(String::valueOf).collect(Collectors.toList()));
-        }
-        return sw.toString();
-    }
+	public String toCSV() throws IOException {
+		final CsvMapper csvMapper = new CsvMapper();
+		try (StringWriter sw = new StringWriter();
+				SequenceWriter sequenceWriter = csvMapper.writer().writeValues(sw)) {
+			// Write headers
+			sequenceWriter.write(columns.stream().map(SQLColumn::getName).toList());
+			// Write rows
+			for (final SQLRow row : rows) {
+				sequenceWriter
+						.write(row.getCells().values().stream().map(String::valueOf).toList());
+			}
+			return sw.toString();
+		}
+	}
 
-    public String toJSON() throws JsonProcessingException {
-        final ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.writeValueAsString(this.rows.stream().map(SQLRow::getCells).collect(Collectors.toList()));
-    }
+	public String toJSON() throws JsonProcessingException {
+		final ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.writeValueAsString(rows.stream().map(SQLRow::getCells).toList());
+	}
 
-    public static class SQLColumn implements Serializable {
-        private static final long serialVersionUID = 953004951165154072L;
+	public static class SQLColumn implements Serializable {
+		private static final long serialVersionUID = 953004951165154072L;
 
-        private final String name;
-        private final String typeName;
-        private final int size;
-        private final int scale;
+		private final String name;
+		private final String typeName;
+		private final int size;
+		private final int scale;
 
-        public SQLColumn(String name, String typeName, int size, int scale) {
-            this.name = name;
-            this.typeName = typeName;
-            this.size = size;
-            this.scale = scale;
-        }
+		public SQLColumn(final String name, final String typeName, final int size, final int scale) {
+			this.name = name;
+			this.typeName = typeName;
+			this.size = size;
+			this.scale = scale;
+		}
 
-        public String getName() {
-            return name;
-        }
+		public String getName() {
+			return name;
+		}
 
-        public String getTypeName() {
-            return typeName;
-        }
+		public String getTypeName() {
+			return typeName;
+		}
 
-        public int getSize() {
-            return size;
-        }
+		public int getSize() {
+			return size;
+		}
 
-        public int getScale() {
-            return scale;
-        }
-    }
+		public int getScale() {
+			return scale;
+		}
+	}
 
-    public static class SQLRow implements Serializable {
-        private static final long serialVersionUID = 3759165044341489985L;
+	public static class SQLRow implements Serializable {
+		private static final long serialVersionUID = 3759165044341489985L;
 
-        private final Map<String, Object> cells = new LinkedHashMap<>();
+		private final Map<String, Object> cells = new LinkedHashMap<>();
 
-        void addCell(String name, Object value) {
-            cells.put(name, value);
-        }
+		void addCell(final String name, final Object value) {
+			cells.put(name, value);
+		}
 
-        public Map<String, Object> getCells() {
-            return cells;
-        }
+		public Map<String, Object> getCells() {
+			return cells;
+		}
 
-        public Object get(String column) {
-            return cells.get(column);
-        }
+		@CheckForNull
+		public Object get(final String column) {
+			return cells.get(column);
+		}
 
-        public String getString(String column) {
-            return String.valueOf(cells.get(column));
-        }
+		@CheckForNull
+		public String getString(final String column) {
+			return String.valueOf(cells.get(column));
+		}
 
-        public int getInt(String column) {
-            return get(column, int.class);
-        }
+		@CheckForNull
+		public Integer getInt(final String column) {
+			return get(column, Integer.class);
+		}
 
-        public double getDouble(String column) {
-            return get(column, double.class);
-        }
+		@CheckForNull
+		public Double getDouble(final String column) {
+			return get(column, Double.class);
+		}
 
-        public float getFloat(String column) {
-            return get(column, float.class);
-        }
+		@CheckForNull
+		public Float getFloat(final String column) {
+			return get(column, Float.class);
+		}
 
-        public short getShort(String column) {
-            return get(column, short.class);
-        }
+		@CheckForNull
+		public Short getShort(final String column) {
+			return get(column, Short.class);
+		}
 
-        public BigDecimal getBigDecimal(String column) {
-            return get(column, BigDecimal.class);
-        }
+		@CheckForNull
+		public BigDecimal getBigDecimal(final String column) {
+			return get(column, BigDecimal.class);
+		}
 
-        public Date getDate(String column) {
-            return get(column, Date.class);
-        }
+		@CheckForNull
+		public Date getDate(final String column) {
+			return get(column, Date.class);
+		}
 
-        public Time getTime(String column) {
-            return get(column, Time.class);
-        }
+		@CheckForNull
+		public Time getTime(final String column) {
+			return get(column, Time.class);
+		}
 
-        public Timestamp getTimeStamp(String column) {
-            return get(column, Timestamp.class);
-        }
+		@CheckForNull
+		public Timestamp getTimeStamp(final String column) {
+			return get(column, Timestamp.class);
+		}
 
-        private <T> T get(String column, Class<T> clazz) {
-            return Optional.ofNullable(cells.get(column))
-                    .filter(clazz::isInstance)
-                    .map(clazz::cast)
-                    .orElse(null);
-        }
-    }
+		private <T> T get(final String column, final Class<T> clazz) {
+			return Optional.ofNullable(cells.get(column))
+					.filter(clazz::isInstance)
+					.map(clazz::cast)
+					.orElse(null);
+		}
+	}
 }
