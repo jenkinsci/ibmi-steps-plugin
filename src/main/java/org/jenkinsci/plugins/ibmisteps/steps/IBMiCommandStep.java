368e4230fad0357217ba041b4b91c97cb6495d7b
Seb Julliand <sebjulliand@gmail.com>
2025-03-21 23:24:36 +0100

    Revert "Fixed indent"
    This reverts commit cf35ffb7089da507d4896ae822dd9f387e6faf6a.

--- src/main/java/org/jenkinsci/plugins/ibmisteps/steps/IBMiCommandStep.java
+++ src/main/java/org/jenkinsci/plugins/ibmisteps/steps/IBMiCommandStep.java
@@ -15,53 +15,53 @@ import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 
 public class IBMiCommandStep extends IBMiStep<CallResult> {
-	private static final long serialVersionUID = 6443392002952411163L;
+    private static final long serialVersionUID = 6443392002952411163L;
 
-	private final String command;
-	private boolean failOnError;
+    private final String command;
+    private boolean failOnError;
 
-	@DataBoundConstructor
-	public IBMiCommandStep(final String command) {
-		this.command = command;
-	}
+    @DataBoundConstructor
+    public IBMiCommandStep(final String command) {
+        this.command = command;
+    }
 
-	public String getCommand() {
-		return command;
-	}
+    public String getCommand() {
+        return command;
+    }
 
-	public boolean isFailOnError() {
-		return failOnError;
-	}
+    public boolean isFailOnError() {
+        return failOnError;
+    }
 
-	@DataBoundSetter
-	public void setFailOnError(final boolean failOnError) {
-		this.failOnError = failOnError;
-	}
+    @DataBoundSetter
+    public void setFailOnError(final boolean failOnError) {
+        this.failOnError = failOnError;
+    }
 
-	@Extension
-	public static class DescriptorImpl extends IBMiStepDescriptor {
-		@Override
-		public String getFunctionName() {
-			return "ibmiCommand";
-		}
+    @Extension
+    public static class DescriptorImpl extends IBMiStepDescriptor {
+        @Override
+        public String getFunctionName() {
+            return "ibmiCommand";
+        }
 
-		@NonNull
-		@Override
-		public String getDisplayName() {
-			return Messages.IBMICommandStep_description();
-		}
-	}
+        @NonNull
+        @Override
+        public String getDisplayName() {
+            return Messages.IBMICommandStep_description();
+        }
+    }
 
-	@Override
-	protected CallResult runOnIBMi(final StepContext context, final LoggerWrapper logger, final IBMi ibmi)
-			throws Exception {
-		logger.log(Messages.IBMICommandStep_running(command));
+    @Override
+    protected CallResult runOnIBMi(final StepContext context, final LoggerWrapper logger, final IBMi ibmi)
+            throws Exception {
+        logger.log(Messages.IBMICommandStep_running(command));
 
-		final CallResult result = ibmi.executeCommand(command);
-		final AS400Message lastMessage = result.getLastMessage();
-		if (result.isSuccessful()) {
-			logger.log(Messages.IBMICommandStep_succeeded(command));
-		} else {
+        final CallResult result = ibmi.executeCommand(command);
+        final AS400Message lastMessage = result.getLastMessage();
+        if (result.isSuccessful()) {
+            logger.log(Messages.IBMICommandStep_succeeded(command));
+        } else {
 			final String error;
 			if (lastMessage != null) {
 				error = Messages.IBMICommandStep_failed_with_message(command, lastMessage.getID(), lastMessage.getText());
@@ -77,6 +77,6 @@ public class IBMiCommandStep extends IBMiStep<CallResult> {
 			}
 		}
 
-		return result;
-	}
+        return result;
+    }
 }
