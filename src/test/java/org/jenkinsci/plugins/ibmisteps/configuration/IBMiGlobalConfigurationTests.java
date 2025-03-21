f5c12c4c7af4e1137f6b1f58246e7f6aabb4dde8
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-03-21 12:46:48 +0100

    Migrate tests to JUnit5
    * Migrate annotations and imports
    * Migrate assertions
    * Remove public visibility for test classes and methods
    * Minor code cleanup

--- src/test/java/org/jenkinsci/plugins/ibmisteps/configuration/IBMiGlobalConfigurationTests.java
+++ src/test/java/org/jenkinsci/plugins/ibmisteps/configuration/IBMiGlobalConfigurationTests.java
@@ -1,44 +1,37 @@
 package org.jenkinsci.plugins.ibmisteps.configuration;
 
-import org.junit.Rule;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 import org.jvnet.hudson.test.recipes.LocalData;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class IBMiGlobalConfigurationTests {
-
-    @Rule
-    public JenkinsRule jenkins = new JenkinsRule();
+@WithJenkins
+class IBMiGlobalConfigurationTests {
 
     @Test
     @LocalData
-    public void loadConfigurationTest() {
+    void loadConfigurationTest(JenkinsRule jenkins) {
         final IBMiGlobalConfiguration configuration = IBMiGlobalConfiguration.get();
         assertNotNull(configuration);
 
         final IBMiServerConfiguration server1 = configuration.getServer("IBMi 1");
-        assertNotNull("Server IBMi 1 must exist", server1);
-        assertEquals(new IBMiServerConfiguration("IBMi 1",
-                        "ibmi1.sebjulliand.github.com",
-                        "a6d5ecee-c167-4ec5-8b5e-ee39ddb5c96e",
-                        "",
-                        true),
+        assertNotNull(server1, "Server IBMi 1 must exist");
+        assertEquals(
+                new IBMiServerConfiguration(
+                        "IBMi 1", "ibmi1.sebjulliand.github.com", "a6d5ecee-c167-4ec5-8b5e-ee39ddb5c96e", "", true),
                 server1);
 
         final IBMiServerConfiguration server2 = configuration.getServer("IBMi 2");
-        assertNotNull("Server IBMi 2 must exist", server2);
-        assertEquals(new IBMiServerConfiguration("IBMi 2",
-                        "ibmi2.sebjulliand.github.com",
-                        "hello-world",
-                        "37",
-                        false),
+        assertNotNull(server2, "Server IBMi 2 must exist");
+        assertEquals(
+                new IBMiServerConfiguration("IBMi 2", "ibmi2.sebjulliand.github.com", "hello-world", "37", false),
                 server2);
 
         final IBMiServerConfiguration notFoundServer = configuration.getServer("NotFound");
-        assertNull("Server 'NotFound' doesn't exist", notFoundServer);
+        assertNull(notFoundServer, "Server 'NotFound' doesn't exist");
     }
 }
