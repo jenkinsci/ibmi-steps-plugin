93f88b95f93286b245202958f3d22df0e89ba0b0
Seb Julliand <sebjulliand@gmail.com>
2025-04-10 11:19:23 +0200

    Added traces for spooled files handler type

--- src/main/java/org/jenkinsci/plugins/ibmisteps/model/IBMi.java
+++ src/main/java/org/jenkinsci/plugins/ibmisteps/model/IBMi.java
@@ -1,13 +1,15 @@
 package org.jenkinsci.plugins.ibmisteps.model;
 
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
+import com.ibm.as400.access.*;
+import edu.umd.cs.findbugs.annotations.CheckForNull;
+import hudson.FilePath;
+import hudson.Util;
+import hudson.util.Secret;
+import org.jenkinsci.plugins.ibmisteps.Messages;
+
 import java.beans.PropertyVetoException;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
-import java.io.Serializable;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -16,32 +18,6 @@ import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
-
-import org.jenkinsci.plugins.ibmisteps.Messages;
-
-import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
-import com.ibm.as400.access.AS400;
-import com.ibm.as400.access.AS400JDBCConnection;
-import com.ibm.as400.access.AS400JDBCDriver;
-import com.ibm.as400.access.AS400JDBCStatement;
-import com.ibm.as400.access.AS400Message;
-import com.ibm.as400.access.AS400SecurityException;
-import com.ibm.as400.access.CharConverter;
-import com.ibm.as400.access.CommandCall;
-import com.ibm.as400.access.ConnectionEvent;
-import com.ibm.as400.access.ConnectionListener;
-import com.ibm.as400.access.ErrorCompletingRequestException;
-import com.ibm.as400.access.IFSFile;
-import com.ibm.as400.access.IFSFileInputStream;
-import com.ibm.as400.access.IFSFileOutputStream;
-import com.ibm.as400.access.Job;
-import com.ibm.as400.access.ObjectDoesNotExistException;
-import com.ibm.as400.access.SecureAS400;
-
-import edu.umd.cs.findbugs.annotations.CheckForNull;
-import hudson.FilePath;
-import hudson.Util;
-import hudson.util.Secret;
 
 public class IBMi implements ConnectionListener, AutoCloseable, Serializable {
 	private static final long serialVersionUID = -3164250407732394897L;
@@ -64,7 +40,7 @@ public class IBMi implements ConnectionListener, AutoCloseable, Serializable {
 	private SpooledFileHandler spooledFileHandler;
 
 	public IBMi(final PrintStream stream, final String host, final StandardUsernamePasswordCredentials credentials,
-			final int ccsid, final boolean secure, final boolean doTrace) throws IOException, InterruptedException {
+	            final int ccsid, final boolean secure, final boolean doTrace) throws IOException, InterruptedException {
 		logger = new LoggerWrapper(stream, doTrace);
 		ibmiConnection = secure ? new SecureAS400() : new AS400();
 		try {
@@ -135,7 +111,7 @@ public class IBMi implements ConnectionListener, AutoCloseable, Serializable {
 
 		if (!isSYSBAS(iASP)) {
 			try (final Connection connection = new AS400JDBCDriver().connect(ibmiConnection);
-					final Statement statement = connection.createStatement()) {
+			     final Statement statement = connection.createStatement()) {
 				try (final ResultSet resultSet = statement.executeQuery(String.format(
 						"Select RDB_NAME From QSYS2.ASP_INFO Where DEVICE_DESCRIPTION_NAME = '%s' Fetch First row only",
 						iASP))) {
@@ -299,10 +275,8 @@ public class IBMi implements ConnectionListener, AutoCloseable, Serializable {
 	}
 
 	/**
-	 * @param query
-	 *            a SQL query
-	 * @param rowProcessor
-	 *            a processor that will run a process on each row
+	 * @param query        a SQL query
+	 * @param rowProcessor a processor that will run a process on each row
 	 * @return <code>true</code> if at least one row was processed, <code>false</code> otherwise.
 	 * @throws SQLException
 	 * @throws AS400SecurityException
@@ -368,7 +342,7 @@ public class IBMi implements ConnectionListener, AutoCloseable, Serializable {
 	public long download(final IFSFile from, final FilePath to)
 			throws IOException, AS400SecurityException, InterruptedException {
 		try (InputStream input = new BufferedInputStream(new IFSFileInputStream(from));
-				OutputStream output = new BufferedOutputStream(to.write())) {
+		     OutputStream output = new BufferedOutputStream(to.write())) {
 			return copy(input, output);
 		}
 	}
@@ -379,7 +353,7 @@ public class IBMi implements ConnectionListener, AutoCloseable, Serializable {
 			to.getParentFile().mkdirs();
 		}
 		try (InputStream input = new BufferedInputStream(from.read());
-				OutputStream output = new BufferedOutputStream(new IFSFileOutputStream(to))) {
+		     OutputStream output = new BufferedOutputStream(new IFSFileOutputStream(to))) {
 			return copy(input, output);
 		}
 	}
@@ -405,10 +379,18 @@ public class IBMi implements ConnectionListener, AutoCloseable, Serializable {
 			} catch (final InterruptedException e) {
 				Thread.currentThread().interrupt();
 				logger.log(Messages.IBMi_failed_sql_service_check(e.getLocalizedMessage()));
-			} catch (SQLException | AS400SecurityException | ObjectDoesNotExistException | IOException | ErrorCompletingRequestException e) {
+			} catch (SQLException | AS400SecurityException | ObjectDoesNotExistException | IOException |
+			         ErrorCompletingRequestException e) {
 				logger.log(Messages.IBMi_failed_sql_service_check(e.getLocalizedMessage()));
 			}
-			spooledFileHandler = checkCount.get() == 2 ? new SQLSpooledFilehandler() : new CLSpooledFilehandler();
+
+			if (checkCount.get() == 2) {
+				logger.trace("Using SQL spooled files handler");
+				spooledFileHandler = new SQLSpooledFilehandler();
+			} else {
+				logger.trace("Using CL spooled files handler");
+				spooledFileHandler = new CLSpooledFilehandler();
+			}
 		}
 		return spooledFileHandler;
 	}
